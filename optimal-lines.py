1f1fc3e112875e62e8ae3c1374ca4f09de0e139d
Todd Wolfson <todd@twolfson.com>
2013-08-08 23:57:11 -0700

    Arriving at the sad discovery of a lack of a full highlighting API =(

--- optimal-lines.py
+++ optimal-lines.py
@@ -13,9 +13,19 @@ class OptimalLinesListener(sublime_plugin.EventListener):
         self.highlight_lines(view)
 
     def highlight_lines(self, view):
+        # For each line
+            # If the line is empty, skip it
+
+            # Find the starting character
+
+            # Count 65 characters from the character
+            # TODO: Make this font-size and settings based
+
+            # TODO: Figure out how to highlight past a character
         # TODO: Don't highlight quick panel or Find in Files
-        regions = [sublime.Region(0, 5)]
-        view.add_regions('optimize_lines_highlight',
-                         regions,
-                         'markup.inserted',
-                         sublime.HIDE_ON_MINIMAP)
+
+        # regions = [sublime.Region(0, 5)]
+        # view.add_regions('optimize_lines_highlight',
+        #                  regions,
+        #                  'markup.inserted',
+        #                  sublime.HIDE_ON_MINIMAP)
